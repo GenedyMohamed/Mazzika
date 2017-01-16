7ec3169b1e1daa39d9a01502c5ea13ec5cf5cbb7
Genedy <genedymohamed96@gmail.com>
2017-01-17 00:49:16 +0200

    returning user and password reset migrations as they were

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
-            $table->string('likes');
             $table->rememberToken();
             $table->timestamps();
         });
