3873759a09c70b55088a965e23b6b08527175644
Mazen <mazenmagdy96@yahoo.com>
2017-01-23 20:25:32 +0200

    Added Bare bones non-functional view

---
+++ resources/views/artist/artist.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section('title')
+    Artist
+@stop
+
+@section('navbar')
+    <nav class="navbar navbar-default">
+        <a href="" class="navbar-brand">My Music</a>
+        <a href="" class="navbar-brand">Upload</a>
+        <a href="" class="navbar-brand">Delete</a>
+        <a href="" class="navbar-brand">Update</a>
+    </nav>
+@stop
+
+@section('footer')
+    <footer class="navbar-fixed-bottom">
+        <p class="text-center">
+            TechHub © 2017
+        </p>
+    </footer>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/artist', function () {
+    return view('/artist/artist');
+});
