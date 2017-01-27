9fa7652b1209a8a6f2736f7da5ad7496a4a0f8df
Abdullah Emad <user.name>
2017-01-27 16:55:56 +0200

    I changed the layout view. I factored out the css in a .css file located in /public/css/layout.css to have a better design. I also add a line of code yield('style') which allows the other pages to add it's nessecary style sheets to avoid sending extra unnessecary data from the server

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,87 +4,13 @@
     <title>Document</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Raleway', sans-serif;
-            font-weight: 100;
-            height: 100vh;
-            margin: 0;
-        }
 
-        .full-height {
-            height: 100vh;
-        }
+<!-- this is the css for the layout -->
+<link rel = "stylesheet" href="/css/layout.css" type="text/css"/>
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
+<!--Here goes any neccessary style sheets -->
+@yield('style')
 
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 12px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-        html,
-        body {
-            height: 100%;
-            /* The html and body elements cannot have any padding or margin. */
-        }
-
-        /* Wrapper for page content to push down footer */
-        #wrap {
-            min-height: 100%;
-            height: auto !important;
-            height: 100%;
-            /* Negative indent footer by its height */
-            margin: 0 auto -50px;
-            /* Pad the bottom by footer height */
-            padding: 0 0 50px;
-        }
-
-        /* Set the fixed height of the footer here */
-        footer {
-            height: 50px;
-            background-color: #090909;
-        }
-
-
-        /* Custom page CSS
-        -------------------------------------------------- */
-        footer > p {
-            padding: 0 125px;
-        }
-    </style>
 </head>
 <body>
     <div id = "wrap">
